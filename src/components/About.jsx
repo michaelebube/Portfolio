11fded95e81300c63e9ec1b62147f8e43d907873
Michael Ebube <michaelebube18@gmail.com>
2025-09-14 17:31:51 +0100

    Reduce icon size and text size on mobile screens

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -58,7 +58,7 @@ const About = () => {
            
            <div className='md:flex items-center justify-between'>
             <p className='pt-14 sm:pt-[72px] md:pt-20 lg:pt-[72px]
-            xl:pt-20 text-sm sm:text-[15px] md:text-[15px] lg:text-[16px] xl:text-[21px]  text-start w-[90%] sm:w-[90%]  md:w-[55%] xl:w-[60%] font-mina leading-loose text-[#BDB9B9]'>I'm a passionate front-end web development student with a solid foundation in HTML, CSS, JavaScript, TypeScript, React, Vue, Nuxt and TailwindCSS. Eager to keep growing, I'm excited to contribute to a team where I can apply my skills and learn from new challenges. My drive for creating visually appealing, user-friendly websites keeps me exploring innovative tools and techniques. Ready to collaborate and make a meaningful impact on projects!</p>
+            xl:pt-20 text-[13px] sm:text-[15px] md:text-[15px] lg:text-[16px] xl:text-[21px]  text-start w-[90%] sm:w-[90%]  md:w-[55%] xl:w-[60%] font-mina leading-loose text-[#BDB9B9]'>I'm a passionate front-end web development student with a solid foundation in HTML, CSS, JavaScript, TypeScript, React, Vue, Nuxt and TailwindCSS. Eager to keep growing, I'm excited to contribute to a team where I can apply my skills and learn from new challenges. My drive for creating visually appealing, user-friendly websites keeps me exploring innovative tools and techniques. Ready to collaborate and make a meaningful impact on projects!</p>
 
             <div className='flex items-center justify-center sm:block'>
                 <img src={profilePic} alt="" className='max-w-[150px]  sm:max-w-[250px] pt-7 sm:pt-10 md:pt-0  sm:ml-[25%] md:ml-0 md:max-w-[250px] lg:max-w-[280px]
@@ -77,8 +77,8 @@ const About = () => {
                         {icons.map((icon) => (
                             <img key={icon.alt} src={icon.src} alt={icon.alt}   className={`
     ${icon.alt === 'Tailwind'
-      ? 'w-[65px] sm:w-[120px] md:w-[90px] lg:w-[120px]'
-      : 'w-[20px] sm:w-[35px] md:w-[25px] lg:w-[40px]'
+      ? 'w-[60px] sm:w-[120px] md:w-[90px] lg:w-[120px]'
+      : 'w-[18px] sm:w-[35px] md:w-[25px] lg:w-[40px]'
     }
   `} />
                         ))}
